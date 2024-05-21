e8d89dbcd3482f13a96ae23b36b98cce6cbc0224
nisol <nisolmedinaue@gmail.com>
2024-05-21 16:43:35 +0200

    categoryDresses

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 import HomePage from './components/HomePage'
+import CategoryDresses from './components/CategoryDresses'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
+    <Routes>
+      <Route>
     <HomePage />
+    <CategoryDresses/>
+      </Route>
+    </Routes>
     </>
   )
 }
