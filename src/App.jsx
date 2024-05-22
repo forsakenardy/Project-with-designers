c499e4685febd5d2a136ecaf048a03e9c1296515
Arnaldo Mera <nardy.allstars@gmail.com>
2024-05-22 18:07:33 +0200

    menu functional

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,23 @@ import Bag from "./assets/images/Bag.svg"
 import { Route, Routes, Link } from 'react-router-dom'
 import SummerMantra from './components/Summer-Mantra'
 import CategoryDresses from './components/CategoryDresses'
+import close from "./assets/images/close.svg"
+import plus from "./assets/images/plus.svg"
+import { useState } from 'react'
 
 
 function App() {
 
+  const [isMenuPressed, setIsMenuPressed] = useState(false);
+  const [isPlusPressed, setIsPlusPressed] = useState(false);
+
+  const toggleClass = () => {
+    setIsMenuPressed((prevState) => !prevState);
+  };
+
+  const toggleClass2 = () => {
+    setIsPlusPressed((prevState) => !prevState);
+  };
 
   return (
     <>
@@ -20,7 +33,7 @@ function App() {
         <div className="icons1">
           <div className="icons1-1">
             <div className="icons1-1-1">
-              <img className="menu" src={Menu} alt="" />
+              <img onClick={toggleClass} className="menu" src={Menu} alt="" />
             </div>
           </div>
           <div className="icons1-2">
@@ -34,8 +47,8 @@ function App() {
           <img className="bag" src={Bag} alt="" />
         </div>
       </div>
- 
-      <Link to='/'/>
+
+      <Link to='/' />
       <Link to='/dress1' />
       <Link to='/categoryDresses' />
       <Routes>
@@ -43,6 +56,34 @@ function App() {
         <Route path='/dress1' element={<SummerMantra />} />
         <Route path='/categoryDresses' element={<CategoryDresses />} />
       </Routes>
+
+      <div className={isMenuPressed ? 'menu-show' : 'hidden'}>
+        <img onClick={toggleClass} className='close' src={close} alt="" />
+        <h1 className='without-line-height'>HOME</h1>
+        <hr />
+        <div className='allcategories'>
+          <h1 className='without-line-height'>ALL CATEGORIES</h1>
+          <img onClick={toggleClass2} className='plus' src={plus} alt="" />
+        </div>
+        <div className={isPlusPressed ? 'allcategories-show' : 'hidden'}>
+          <h1>SALE - 60%</h1>
+          <h1>BLOUSES</h1>
+          <h1>DRESSES</h1>
+          <h1>JACKETS</h1>
+          <h1>KNITEWEAR</h1>
+          <h1>SHIRTS</h1>
+          <h1>SKIRTS</h1>
+          <h1>TOPS</h1>
+          <h1>TROUSERS</h1>
+          <h1>ACCESORIES</h1>
+        </div>
+        <hr />
+        <h1 className='without-line-height'>ACCOUNT</h1>
+        <hr />
+        <h1 className='without-line-height'>Contact</h1>
+        <hr />
+        <h1 className='without-line-height'>PROFESSIONAL ACCES</h1>
+      </div>
     </>
   )
 }
