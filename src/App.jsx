f38f8dccf14c9ad2899e5eb131ab8bfa42eb37eb
Arnaldo Mera <nardy.allstars@gmail.com>
2024-05-23 17:16:34 +0200

    logo with acces to menu

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import CategoryDresses from './components/CategoryDresses'
 import AddToBag from './components/AddToBag'
 import close from "./assets/images/close.svg"
 import plus from "./assets/images/plus.svg"
+import less from "./assets/images/Less.svg"
 import { useState } from 'react'
 
 
@@ -21,6 +22,7 @@ function App() {
   const [isMenuPressed, setIsMenuPressed] = useState(false);
   const [isPlusPressed, setIsPlusPressed] = useState(false);
   const [isClosing, setIsClosing] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleClass = () => {
     if (isMenuPressed) {
@@ -38,17 +40,29 @@ function App() {
     setIsPlusPressed((prevState) => !prevState);
   };
 
+  const toggleClass3 = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+
+
   return (
     <>
       <div className="navbar">
         <div className="icons1">
           <div className="icons1-1">
             <div className="icons1-1-1">
-              <img onClick={toggleClass} className="menu" src={Menu} alt="" />
+              <img onClick={() => {
+            toggleClass()
+            toggleClass3()
+          }} className="menu" src={Menu} alt="" />
             </div>
           </div>
           <div className="icons1-2">
-            <img className="logo" src={Logo} alt="" />
+            <img onClick={() => {
+            toggleClass()
+            toggleClass3()
+          }} className="logo" src={Logo} alt="" />
           </div>
         </div>
         <div className="icons2">
@@ -59,14 +73,20 @@ function App() {
         </div>
       </div>
       <div className={isMenuPressed ? (isClosing ? 'menu-show closing' : 'menu-show') : 'hidden'}>
-        <img onClick={toggleClass} className='close' src={close} alt="" />
-        <Link onClick={toggleClass} to='/'><h1 className='without-line-height'>HOME</h1></Link>
+        <img onClick={() => {
+            toggleClass()
+            // toggleClass3()
+          }} className='close' src={close} alt="" />
+        <Link onClick={() => {
+            toggleClass()
+            // toggleClass3()
+          }} to='/'><h1 className='without-line-height'>HOME</h1></Link>
         <hr />
         <div className='allcategories'>
           <h1 className='without-line-height'>ALL CATEGORIES</h1>
-          <img onClick={toggleClass2} className='plus' src={plus} alt="" />
+          <img onClick={toggleClass2} className='plus' src={isPlusPressed ? less : plus} alt='' />
         </div>
-        <div className={isPlusPressed ? 'allcategories-show' : 'hidden'}>
+        <div className={isMenuPressed && !isPlusPressed ?'hidden' :(isPlusPressed ? 'allcategories-show' : 'allcategories-hide ') }>
           <h1>SALE - 60%</h1>
           <h1>BLOUSES</h1>
           <Link onClick={toggleClass} to='/categoryDresses'><h1>DRESSES</h1></Link>
@@ -87,12 +107,12 @@ function App() {
       </div>
 
 
-    
+
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/dress1' element={<SummerMantra />} />
         <Route path='/categoryDresses' element={<CategoryDresses />} />
-        <Route path='/AddToBag' element={<AddToBag/>} />
+        <Route path='/AddToBag' element={<AddToBag />} />
       </Routes>
     </>
   )
